1846598ce560c5be24870c3a3b452e5d30443a0d
Ken Talosig <talosigkenji@gmail.com>
2025-09-30 16:09:53 +0800

    Update main.py
    - refurbished comments
    - merged make_folder to generate_qr
    - removed set_data, included in generated_qr
    - renamed testBinary variable to choice_Feature
    - added return statement for variable value reuse. (for view_qr)

--- main.py
+++ main.py
@@ -1,62 +1,58 @@
-#current version: 1.3
+#current version: 1.3.1
+#made in Python 3.13
 '''
-- Added error catching in functions.
-
-1.3:
-- Binary code feature: Convert your text into Binary.
+1.3.1:
+- Optimizations in code
+- Comments spread evenly.
 '''
 
-# Script uses these following libraries, please install.
-import qrcode # Most important.
-import os # To open folder in different operating systems.
-from PIL import ImageTk # Image view.
-from tkinter import *
+# Script uses these following imports.
+import qrcode
+import os 
+from PIL import ImageTk
+from tkinter import * 
 from tkinter import filedialog, messagebox
 from datetime import datetime
 
-data = None #variable is used for holding data in qr generation
-color = ["White", "Green"] # variable used to store color
-qrCode_folder = "qrcodes" #folder that will be created
+color = ["White", "Green"] # Used for color selection.
+qrCode_folder = "qrcodes"
 
-#---FUNCTIONS---
+# Variables that will be used for returning value
+date = ""
+fileName = ""
+link = ""
+
+# --- FUNCTIONS ---
 def clear_entry():
     entry.delete(0, END)
 
-def make_folder(): # Block is added for function purposes.
-    try:
-        os.mkdir(qrCode_folder)
-    except FileExistsError:
-        pass
-    except Exception as e:
-        print(f"Debug Error: {e}")
-
-
 def open_folder():
-    if os.name == 'posix': #macOS or Linux
+    if os.name == 'posix': # used for other OS
         os.system(f'open "{qrCode_folder}"')
     else:
         os.startfile(qrCode_folder)
 
-def set_data(): # Preparing the data variable, placing user input into data var.
-    global data
+def generate_qr(): # Main process of generating QR Code
+    global data, date, fileName, link
+    colVar = 0 
+
     data = entry.get()
-
-def generate_qr(): # QR Code Generation process.
-    global data #test
-    colVar = 0
-
-    #following is for testing purposes
-    date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # Gets current date and time, for unique filename.
-    fileName = f"qrcode_{date}.png" #name for every qr code
+    date = datetime.now().strftime("%y%m%d-%H%M%S") # Gets current date and time, for unique filename.
+    fileName = f"qrcode_{date}.png"
     link = f"{qrCode_folder}/{fileName}"
 
-    if testBinary.get() == "1": #testBinary test
+    if choice_Feature.get() == "1": # Will change if there are new variants.
         colVar = 1
         data = ' '.join(format(ord(char), 'b') for char in data)
 
-    make_folder()
+    try: # What if there is no folder yet?
+        os.mkdir(qrCode_folder)
+    except FileExistsError: # Checks to see if folder is already produced.
+        pass
+    except Exception as e: # Checks other errors, basically.
+        print(f"Debug Error: {e}")
 
-    try:
+    try: # The QR code is being created.
         qr = qrcode.QRCode(version=5, box_size=3, border=4)
         qr.add_data(data)
         qr.make(fit=True)
@@ -64,52 +60,50 @@ def generate_qr(): # QR Code Generation process.
         image = qr.make_image(fill="Black", back_color=color[colVar])
         image.save(link) # Saves with unique filename.
 
-    except Exception as e: #catch error if cap
+    except Exception as e: # Uh oh, QR Code isn't made properly, we want user to see.
         messagebox.showerror("Error", f"Failed to create QR Code: {e}\n\nActually, what did you do to get here?") # Shows error.
+    
+    return date, fileName, link # QR Code is finished! We transfer it back to starting variables
 
+def browse_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")]) # What if user wants to add in text file instead?
 
-def browse_file(): # Function to browse and read text file content
-    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
-
-    if file_path: # File selected becomes entry data.
+    if file_path:
         with open(file_path, 'r') as file:
-            file_to_Text = file.read()
-        entry.delete(0, END)
-        entry.insert(0, file_to_Text)
+            file_to_Text = file.read() # It turns the text file into string
+        entry.delete(0, END) # Deletes what's inside of entry first.
+        entry.insert(0, file_to_Text) 
 
-def view_qr(): # Window used for QR Code view.
-    #following is for testing purposes
-    date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # Gets current date and time, for unique filename.
-    fileName = f"qrcode_{date}.png" #name for every qr code
-    link = f"{qrCode_folder}/{fileName}"
+def view_qr(): # Pops up a window for user to see QR Code.
+    global date, fileName, link
 
     # SETUP FOR VIEW
     top = Toplevel()
     top.title("Generated QR Code")
+    top.resizable(False, False)
 
     labelset = Label(top, text="QR CODE GENERATED:", font=("Helvetica", 12))
     labelset.pack(pady=10)
 
-    try: #this is to try code if it shows picture error.
-        qr_image = ImageTk.PhotoImage(file=link) # test for link variable
+    try: # User wants to see QR Code.
+        qr_image = ImageTk.PhotoImage(file=link) # Checks if QR Code is in folder.
         label = Label(top, image=qr_image)
-        label.image = qr_image  # keep a reference!
+        label.image = qr_image  # For safekeeping purposes.
         label.pack(pady=15, padx=15)
     except Exception as e:
         messagebox.showerror("Error", f"Failed to show QR Code: {e}\n\nMight need to deal with that.")
         labelset.config(text="ERROR, NO QR CODE FOUND.")
 
-    top.after(10000, lambda: top.destroy())  # auto-close after 10 seconds
+    top.after(10000, lambda: top.destroy())  # QR Code doesn't need to be there for long, so user has 10 seconds to screenshot the QR Code before it dies.
 
-#---GUI SETUP---
-#main window
+#  ---  GUI SETUP  ---
 main = Tk()
 main.title("QR Code Generator")
 main.resizable(False, False)
 
 frame1 = Frame(main)
 frame2 = Frame(main)
-subframe1 = Frame(frame1) #for features selection
+subframe1 = Frame(frame1) # To add in a separate features area.
 
 frame1.grid(row=0, column=0, padx=20, pady=20)
 frame2.grid(row=0, column=1, padx=20, pady=20)
@@ -119,12 +113,25 @@ label_title = Label(frame1, text="QR Code Generator", font=('Arial', 20))
 label_desc = Label(frame1, text="Enter anything to generate QR Code")
 entry = Entry(frame1, width=30, font=('Arial', 17))
 label_features = Label(frame1, text="Features")
-buttonGenerate = Button(frame1, text="Generate QR Code", command=lambda: [set_data(), generate_qr(), view_qr()], font=('Arial', 15))
+buttonGenerate = Button(frame1, text="Generate QR Code", command=lambda: [generate_qr(), view_qr()], font=('Arial', 15))
 
 #subframe1 contents
-testBinary = StringVar(value="0")
-
-binaryCheck = Checkbutton(subframe1, variable=testBinary, text="Binary").grid(row=0, column=0)
+choice_Feature = StringVar(value="0")
+for featTxt, val, rowNum, colNum in [ # In loop to optimize, and it looks good in readability.
+    ("Normal", "0", "0", "0"),
+    ("Binary", "1", "0", "1"),
+]:
+    Radiobutton(
+        subframe1, 
+        variable=choice_Feature, 
+        text=featTxt, 
+        value=val
+    ).grid(
+        row=rowNum,
+        column=colNum,
+        padx=2,
+        pady=2
+    )
 
 #pack for frame1
 label_title.pack()
@@ -135,11 +142,13 @@ label_features.pack(pady=5)
 subframe1.pack(pady=5, fill="x", expand=True)
 
 #pack for subframe1
+#nothing.
 
 #frame2 contents
 label_file = Label(frame2, text="Or, you can enter a text file to generate QR code from its content.", wraplength=250, justify="center", font=('Helvetica', 10))
 label_file.pack(pady=10)
-for textIn, btnCommand in [
+
+for textIn, btnCommand in [ # In loop to optimize, and it looks good in readability.
     ("Browse File", browse_file),
     ("Clear Entry", clear_entry),
     ("Open Storage Folder", open_folder)
